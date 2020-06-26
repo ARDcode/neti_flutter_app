a48b8890bdbb47f81439226be5302e1692b73928
Айнур Динмухаметов <codeard@gmail.com>
2020-06-26 08:59:01 +0300

    lesson3

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -11,6 +11,7 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   TestAPI _testAPI = TestAPI();
   ResponseModel _data = ResponseModel();
+  int _currentIndex = 0;
   @override
   void initState() {
     super.initState();
@@ -28,26 +29,152 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          Result item = _data.results[index];
-          return ListTile(
-            onTap: () {
-              Navigator.of(context, rootNavigator: true).push(
-                MaterialPageRoute(
-                  builder: (context) => PersonPage(
-                    person: item,
-                  ),
-                ),
+      appBar: AppBar(
+        title: Text('AppBar'),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Center(child: Text('DrawerHeader')),
+              decoration: BoxDecoration(color: Colors.redAccent),
+            ),
+            Text('FirstPage'),
+            Text('SecondsPage')
+          ],
+        ),
+      ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (context) {
+                return Content();
+              });
+        },
+        child: Text('GO'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: true,
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.ac_unit),
+              title: Text('1'),
+              backgroundColor: Colors.red),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('2'),
+              backgroundColor: Colors.black),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('2'),
+              backgroundColor: Colors.black),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('2'),
+              backgroundColor: Colors.black)
+        ],
+      ),
+      body: IndexedStack(
+        children: [
+          ListView.builder(
+            itemBuilder: (context, index) {
+              Result item = _data.results[index];
+              return ListTile(
+                onTap: () {
+                  Navigator.of(context, rootNavigator: true).push(
+                    MaterialPageRoute(
+                      builder: (context) => PersonPage(
+                        person: item,
+                      ),
+                    ),
+                  );
+                },
+                leading: Hero(
+                    child: Image.network(item.image), tag: item.id.toString()),
+                title: Text(item.name, style: TextStyle(color: Colors.black)),
+                subtitle: Text(item.type),
               );
             },
-            leading:
-                Hero(child: Image.network(item.image), tag: item.id.toString()),
-            title: Text(item.name, style: TextStyle(color: Colors.black)),
-            subtitle: Text(item.type),
-          );
-        },
-        itemCount: _data.results != null ? _data.results.length : 0,
+            itemCount: _data.results != null ? _data.results.length : 0,
+          ),
+          Container(
+            child: Text('second Tab'),
+          )
+        ],
+        index: _currentIndex,
+      ),
+    );
+  }
+}
+
+class Content extends StatelessWidget {
+  const Content({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      heightFactor: 0.4,
+      child: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.message),
+            title: Text('Messages'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Persons'),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.message),
+            title: Text('Messages'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Persons'),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.message),
+            title: Text('Messages'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Persons'),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.message),
+            title: Text('Messages'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Persons'),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
       ),
     );
   }
